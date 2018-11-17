33187e6b9236b538644b894f7c4cc231ccd66102
Eduardodsb <eduardo_b_bosa@hotmail.com>
2018-11-17 18:14:46 -0200

    #Pessoa

--- src/com/ms/sigada/model/Pessoa.java
+++ src/com/ms/sigada/model/Pessoa.java
@@ -4,10 +4,17 @@ public class Pessoa {
 	private String nome, cpf, dataDeNascimento, endereco1, endereco2;
 	private int matricula, idRefeitorio;
 	
-	public void definir_nome(String nome) {
+	public void setNome(String nome) {
 		this.nome = (nome == null ? "" : nome);
 	}
-	public String obter_nome() {
+	public String getNome() {
 		return this.nome;
+	}
+	
+	public void setCPF(String cpf) {
+		this.cpf = (cpf == null ? "" : cpf);
+	}
+	public String getCPF() {
+		return this.cpf;
 	}
 }
