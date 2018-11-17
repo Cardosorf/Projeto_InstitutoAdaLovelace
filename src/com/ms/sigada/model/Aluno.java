1ab33ed1e5ba3edc62e1aabec9de61a7ec4ec23e
Rafael Cardoso <cardoso.rf@gmail.com>
2018-11-17 19:15:00 -0200

    #Aluno

--- src/com/ms/sigada/model/Aluno.java
+++ src/com/ms/sigada/model/Aluno.java
@@ -1,5 +1,12 @@
 package com.ms.sigada.model;
 
-public class Aluno {
-
+public class Aluno extends Pessoa{
+	
+	private String telefoneResponsavel1, telefoneResponsavel2, nomeResponsavel1, nomeResponsavel2;
+	
+	public void setTelefoneResponsavel1(String tel1) {
+		this.telefoneResponsavel1 = (tel1 == null ? "" : tel1);
+	}
+	
+	
 }
