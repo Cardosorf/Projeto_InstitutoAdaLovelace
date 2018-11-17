d8265927bf6591ede6842eee1ebcfb6857497475
Eduardodsb <eduardo_b_bosa@hotmail.com>
2018-11-17 18:58:30 -0200

    #Aluno/Pessoa

--- src/com/ms/sigada/model/Pessoa.java
+++ src/com/ms/sigada/model/Pessoa.java
@@ -1,8 +1,8 @@
 package com.ms.sigada.model;
 
 public class Pessoa {
-	private String nome, cpf, dataDeNascimento, endereco1, endereco2;
-	private int matricula, idRefeitorio;
+	private String nome, CPF, dataDeNascimento, endereco1, endereco2;
+	private int matricula, IDRefeitorio;
 	
 	public void setNome(String nome) {
 		this.nome = (nome == null ? "" : nome);
@@ -11,10 +11,43 @@ public class Pessoa {
 		return this.nome;
 	}
 	
-	public void setCPF(String cpf) {
-		this.cpf = (cpf == null ? "" : cpf);
+	public void setCPF(String CPF) {
+		this.CPF = (CPF == null ? "" : CPF);
 	}
 	public String getCPF() {
-		return this.cpf;
+		return this.CPF;
 	}
+	
+	public void setDataDeNascimento(String dataDeNascimento) {
+		this.dataDeNascimento = (dataDeNascimento == null ? "" : dataDeNascimento);
+	}
+	public String getDataDeNascimento() {
+		return this.dataDeNascimento;
+	}
+	
+	public String getEndereco1() {
+		return endereco1;
+	}
+	public void setEndereco1(String endereco1) {
+		this.endereco1 = (endereco1 == null ? "" : endereco1);
+	}
+	public String getEndereco2() {
+		return endereco2;
+	}
+	public void setEndereco2(String endereco2) {
+		this.endereco2 = (endereco2 == null ? "" : endereco2);
+	}
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public int getIDRefeitorio() {
+		return IDRefeitorio;
+	}
+	public void setIDRefeitorio(int iDRefeitorio) {
+		IDRefeitorio = iDRefeitorio;
+	}
+	
 }
