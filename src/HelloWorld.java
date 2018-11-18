432218f2f4362b085494189e8e635563da46db8a
Rafael Cardoso <cardoso.rf@gmail.com>
2018-11-18 00:02:34 -0200

    +Aluno DAO, Aluno.txt

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,17 @@
+import com.ms.sigada.dao.AlunoDAO;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 
+		AlunoDAO teste = new AlunoDAO();
+		
+		
 	}
 
+	
+	
+	
+	
 }
